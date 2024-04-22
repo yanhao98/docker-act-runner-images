3e1c61c1d9533535233091e0f95cce6b4f5c42c7
严浩 <me@yanhao.ren>
2024-04-22 18:33:47 +0800

    [no ci] Update Dockerfile.pnpm to run pnpm --version interactively

--- test/run-build.sh
+++ test/run-build.sh
@@ -4,4 +4,4 @@ docker build \
     --build-arg NODE_VERSION=16.20.0 \
     --build-arg PNPM_VERSION=9.0.0 \
     -t yanhao98/runner-images:pnpm9-node16 -f Dockerfile.pnpm .
-docker run --rm yanhao98/runner-images:pnpm9-node16 pnpm --version
+docker run -it --rm yanhao98/runner-images:pnpm9-node16 pnpm --version
