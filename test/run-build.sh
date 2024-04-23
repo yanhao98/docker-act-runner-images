4b0b52888af30b7b54a1b2d9fff91e01992a671a
oo1dev <i@oo1.dev>
2024-04-23 23:02:45 +0800

    Update Dockerfile.pnpm to include PNPM_HOME environment variable and add it to PATH

--- test/run-build.sh
+++ test/run-build.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -eux
 
 docker build \
     --build-arg NODE_VERSION=16.0.0 \
     --build-arg PNPM_VERSION=9.0.0 \
     -t yanhao98/runner-images:pnpm9-node16 -f Dockerfile.pnpm .
-docker run -it --rm yanhao98/runner-images:pnpm9-node16 pnpm --version
-docker run -it --rm yanhao98/runner-images:pnpm9-node16 node --version
-docker run -it --rm yanhao98/runner-images:pnpm9-node16 uname -a
+docker run -it --rm yanhao98/runner-images:pnpm9-node16 bash --noprofile --norc -e -o pipefail -c "pnpm --version"
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 uname -v
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 cat /etc/os-release
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 env
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 cat /etc/debian_version # 12.5
