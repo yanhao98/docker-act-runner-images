7abc275d272374fd64884d11bf5915dd7f66f945
oo1dev <i@oo1.dev>
2024-04-24 23:40:48 +0800

    Update Dockerfile.pnpm to use bookworm-slim base image and optimize build process

--- test/run-build.sh
+++ test/run-build.sh
@@ -4,8 +4,8 @@ docker build \
     --build-arg NODE_VERSION=16.0.0 \
     --build-arg PNPM_VERSION=9.0.0 \
     -t yanhao98/runner-images:pnpm9-node16 -f Dockerfile.pnpm .
+
+docker image inspect yanhao98/runner-images:pnpm9-node16 --format='{{.Size}}'
 docker run -it --rm yanhao98/runner-images:pnpm9-node16 bash --noprofile --norc -e -o pipefail -c "pnpm --version"
-# docker run -it --rm yanhao98/runner-images:pnpm9-node16 uname -v
-# docker run -it --rm yanhao98/runner-images:pnpm9-node16 cat /etc/os-release
 # docker run -it --rm yanhao98/runner-images:pnpm9-node16 env
-# docker run -it --rm yanhao98/runner-images:pnpm9-node16 cat /etc/debian_version # 12.5
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 du -sh /var/cache /var/log /var/lib/apt/lists /tmp
