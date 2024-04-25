7763d92e194d3502c4583d66630ac99c0e858d15
严浩 <me@yanhao.ren>
2024-04-25 14:01:07 +0800

    Optimize Dockerfile.pnpm build process and update base image to bookworm-slim

--- test/run-build.sh
+++ test/run-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -eux
-
 docker build \
     --build-arg NODE_VERSION=16.0.0 \
     --build-arg PNPM_VERSION=9.0.0 \
     -t yanhao98/runner-images:pnpm9-node16 -f Dockerfile.pnpm .
 
 docker image inspect yanhao98/runner-images:pnpm9-node16 --format='{{.Size}}'
+docker image history yanhao98/runner-images:pnpm9-node16
 docker run -it --rm yanhao98/runner-images:pnpm9-node16 bash --noprofile --norc -e -o pipefail -c "pnpm --version"
+docker run -it --rm yanhao98/runner-images:pnpm9-node16 pnpm store path
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 bash -c "ls -lhA --color=auto /pnpm"
 # docker run -it --rm yanhao98/runner-images:pnpm9-node16 env
-# docker run -it --rm yanhao98/runner-images:pnpm9-node16 du -sh /var/cache /var/log /var/lib/apt/lists /tmp
+# docker run -it --rm yanhao98/runner-images:pnpm9-node16 du -sh /var/cache /var/log /var/lib/apt/lists /tmp /pnpm/store
